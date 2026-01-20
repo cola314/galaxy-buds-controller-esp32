e75a29b468d36f2f9f21313f860a9ba3938b16d8
cola314 <ubj0314@naver.com>
2026-01-21 00:51:45 +0900

    Fix PendingCommand struct redefinition error

--- esp32_ble_spp_bridge/ble_server.h
+++ esp32_ble_spp_bridge/ble_server.h
@@ -70,12 +70,8 @@ class CommandCallbacks: public BLECharacteristicCallbacks {
     }
 };
 
-// 보류 중인 노이즈 컨트롤 명령
-struct PendingCommand {
-    bool has_command;
-    uint8_t data[2];
-    int len;
-};
+// 보류 중인 노이즈 컨트롤 명령 (main .ino에서 정의)
+struct PendingCommand;
 extern PendingCommand pending_noise_cmd;
 
 class NoiseControlCallbacks: public BLECharacteristicCallbacks {
